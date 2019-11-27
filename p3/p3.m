14fcba275e67f42d855ff2efd239a28dbbbbb990
luiserh1 <luis.serranohernandez.1415@gmail.com>
2019-11-27 14:25:52 +0100

    Hasta el punto 2.4 empezado, no depurado

--- p3/p3.m
+++ p3/p3.m
@@ -1,15 +1,21 @@
+#######
+# 2.1 #
+#######
 # Cargado de datos del Corpus Hart
 load data/hart/tr.dat;
 load data/hart/trlabels.dat;
 load data/hart/ts.dat;
 load data/hart/tslabels.dat;
 
+#######
+# 2.2 #
+#######
 # Trasponemos los datos para adaptalos a la biblioteca
 # nnet trabaja con los datos por columnas, no por filas
-mInput = tr’;
-mOutput = trlabels’;
-mTestInput = ts’;
-mTestOutput = tslabels’;
+mInput = tr';
+mOutput = trlabels';
+mTestInput = ts';
+mTestOutput = tslabels';
 
 # La proporcion en el uso de las muestras es:
 # 80% entrenamiento; 20% test
@@ -18,7 +24,7 @@ nTr=floor(nSamples*0.8);
 nVal=nSamples-nTr;
 
 # Barajado aleatorio de las muestras con los indices random
-rand(’seed’,23);
+rand('seed',23);
 indices=randperm(nSamples);
 
 # Seleccion de las primeras muestras para entrenamiento
@@ -28,5 +34,39 @@ mTrainOutput=mOutput(indices(1:nTr));
 mValiInput=mInput(:,indices((nTr+1):nSamples));
 mValiOutput=mOutput(indices((nTr+1):nSamples));
 
+#######
+# 2.3 #
+#######
+# Codificacion de las etiquetas al fomato requerido por MLP
+trainoutDisp = [(mTrainOutput(:,:)==1);(mTrainOutput(:,:)==2)];
+validoutDisp = [(mTrainOutput(:,:)==1);(mTrainOutput(:,:)==2)];
 
+# Normalizacion de los datos (mean = 0; stdDev = 1)
+[mTrainInputN, cMeanInput, cStdInput] = prestd(mTrainInput);
+
+# Estructura especial de representacion
+# P -> Datos de entrada; P -> Datos de salida
+VV.P = mValiInput;
+VV.T = validoutDisp;
+
+# Normalizacion de los datos de validacion partiendo de la media
+# y la desviacion tipica del conjunto de entrenamiento
+VV.P = trastd(VV.P,cMeanInput,cStdInput);
+
+#######
+# 2.4 #
+#######
+# Matriz Dx2 con los valores máximo y mínimo de cada dimensión
+Pr = minmax(mTrainInputN();
+# Un vector fila con el número de neuronas en cada capa oculta y en la capa de salida
+ss = [nHidden, nOutput];
+# Lista de funciones de activacion de cada capa
+trf = {"tansig","logsig"};
+# Algoritmo de entrenamiento de la red neuronal (solo tenemos backprop)
+btf = "trainlm";
+# Parametro que no se emplea en la version actual
+blf = "";
+# Funcion objetivo a minimizar (solo tenemos el error cuadratico medio)
+pf = "mse";
+net = newff(Pr, ss, trf, btf, blf, pf);
 
